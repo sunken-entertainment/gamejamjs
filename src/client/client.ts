3e5ac30e6805bda7f13428a58f11873b65a39735
Daniel Gimbatschki <Daniel.Gimbatschki@student.hpi.uni-potsdam.de>
2016-01-09 21:13:09 +0100

    fixed client js

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,7 +7,7 @@ var Stats = require('stats-js'),
     THREE = require('three');
 
 // modules
-import RenderPipeline = require('render-pipeline');
+import RenderPipeline = require('./render-pipeline');
 
 // global vars
 var renderer, renderPipeline, scene, camera, light, stats, controls;
@@ -18,6 +18,12 @@ export function clearScene() {
     scene = new THREE.Scene();
     scene.add(camera);
     scene.add(light);
+
+    var geometry = new THREE.TorusKnotGeometry(0.5 - 0.12, 0.12);
+    var material = new THREE.MeshNormalMaterial();
+    var mesh: THREE.Mesh = new THREE.Mesh(geometry, material);
+    mesh.scale.set(5, 5, 5);
+    scene.add(mesh);
 
     initEffects();
 }
@@ -54,7 +60,6 @@ render();
 
 function render() {
     requestAnimationFrame(render);
-    controls.update();
     stats.update();
 
     renderPipeline.render();
@@ -76,7 +81,7 @@ function init() {
     // camera
     camera = new THREE.PerspectiveCamera(45, 800 / 400, 1, 50000);
     //camera = new THREE.OrthographicCamera( 800 / - 2, 800 / 2, 400 / 2, 400 / - 2, 1, 50000 );
-    camera.position.z = 25000;
+    camera.position.z = 10;
     camera.up = new THREE.Vector3(0, 0, 1);
     camera.lookAt(new THREE.Vector3());
 
@@ -88,7 +93,6 @@ function init() {
     // stats
     stats = new Stats();
     stats.domElement.setAttribute("id", "stats");
-    stats.domElement.style.display = "none";
     canvasParent().appendChild(stats.domElement);
 
     // render pipeline
--- src/client/render-pipeline.ts
+++ src/client/render-pipeline.ts
@@ -3,9 +3,22 @@
 
 class RenderPipeline {
     private THREE: any;
+    private renderer: any;
+    private camera: any;
+    private scene: any;
 
     constructor(THREE: any) {
         this.THREE = THREE;
+    }
+
+	init(renderer: THREE.Renderer, camera: THREE.Camera, scene: THREE.Scene, width: number, height: number) {
+		this.renderer = renderer;
+		this.camera = camera;
+		this.scene = scene;
+	}
+
+    render() {
+		this.renderer.render(this.scene, this.camera);
     }
 }
 
